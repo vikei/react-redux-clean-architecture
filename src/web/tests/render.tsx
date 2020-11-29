2a6942c39fcba8d387b52efe7d5fdd614c3c1f97
vikei <vikei.dev@gmail.com>
2020-11-29 16:08:59 +0200

    Remove global styles from test func

--- src/web/tests/render.tsx
+++ src/web/tests/render.tsx
@@ -4,14 +4,12 @@ import {ComponentType, ReactElement} from "react";
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
 import createStore from "../../store/main/create-store";
-import GlobalStyles from "../library/components/global-styles";
 import {MessagesProvider} from "../library/hooks/use-messages";
 
 export default function render(ui: ReactElement) {
   const store = createStore();
   const Wrapper: ComponentType = ({children}) => (
     <Provider store={store}>
-      <GlobalStyles />
       <MessagesProvider>
         <BrowserRouter>{children}</BrowserRouter>
       </MessagesProvider>
